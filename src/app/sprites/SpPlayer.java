8c16d4384fd79af2d6c7754abcfe2c98fd5c668a
Jean Carlos Santoya Cabrera <jeancarlosodi@gmail.com>
2020-08-31 07:56:14 -0500

    added new class

---
+++ src/app/sprites/SpPlayer.java
@@ -0,0 +1,15 @@
+package app.sprites;
+
+import services.gameService.GameSprite;
+
+/**
+ *
+ * @author Jean Carlos Santoya Cabrera jeancarlosodi@gmail.com
+ */
+public class SpPlayer extends GameSprite{
+    
+    public SpPlayer() {
+        super(0, 0, "/resources/character/skeleton.png");
+    }
+    
+}
